35f9f80a8309bcd6d6da9c3e4301d9ddfe5536de
Stevie Howard <4117011+stvhwrd@users.noreply.github.com>
2019-11-12 11:37:03 -0800

    Update dotfiles.sh

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -6,26 +6,26 @@
 source ./install/utils.sh
 keep_sudo_alive
 
-# Install Zsh:
-sudo apt -y install zsh
-
-# Install oh-my-zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+# Install prezto
+git clone --recursive https://github.com/stvhwrd/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 # Install some external plugins:
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-completions ~/.prezto/custom/plugins/zsh-completions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 
-git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+/bin/zsh -c  \
+"setopt EXTENDED_GLOB && for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done"
 
 # Set Zsh as your default shell:
 chsh -s /bin/zsh
 
 # Move/replace setting files to /Home
-replace ".gitconfig"        ".gitconfig"
-replace ".gitignore_global" ".gitignore_global"
-replace ".zshrc"            ".zshrc"
+# replace ".gitconfig"        ".gitconfig"
+# replace ".gitignore_global" ".gitignore_global"
+# replace ".zshrc"            ".zshrc"
 
 # Git configs
 e_header "Configure your Git settings: "
